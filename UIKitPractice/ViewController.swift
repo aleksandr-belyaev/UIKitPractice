ea1df7d9e21c80de25ebdd81d7d14369bcd1fe3a
aleksandr-belyaev <belyaev.photo@gmail.com>
2020-12-29 08:21:24 +0700

    Initial Commit

---
+++ UIKitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPractice
+//
+//  Created by Александр Беляев on 29.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
