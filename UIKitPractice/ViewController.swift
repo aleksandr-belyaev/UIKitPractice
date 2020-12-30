a890c61b4123b8ccacdfd9b1004ebc00433a903e
aleksandr-belyaev <belyaev.photo@gmail.com>
2020-12-30 20:41:58 +0700

    Во избежание поломки ноутбука от насильственных действий с моей стороны по причине плывущего в новый год интерфейса занялся аутлетами

--- UIKitPractice/ViewController.swift
+++ UIKitPractice/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var resultField: UILabel!
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var plusMinusButton: UIButton!
+    @IBOutlet weak var procentButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multipleButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var equalsButton: UIButton!
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
